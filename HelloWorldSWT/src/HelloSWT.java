27b2d13a482324c2055e93f225a9fba26d35fd3a
Young <44830681+young107306076@users.noreply.github.com>
2021-11-09 10:29:04 +0800

    Midterm1

---
+++ HelloWorldSWT/src/HelloSWT.java
@@ -0,0 +1,18 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class HelloSWT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Display display=new Display();
+		Shell shell = new Shell(display);
+		shell.setText("Hello World!");
+		shell.open();
+		while(!shell.isDisposed()) {
+			if(!display.readAndDispatch()) display.sleep();
+		}
+		display.dispose();
+	}
+
+}
